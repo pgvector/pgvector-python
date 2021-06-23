0721449821973d19df6496f7839eab4fcad8ff5f
Andrew Kane <andrew@ankane.org>
2021-06-22 22:14:46 -0700

    Improved tests

--- tests/test_psycopg2.py
+++ tests/test_psycopg2.py
@@ -2,23 +2,23 @@ import numpy as np
 from pgvector.psycopg2 import register_vector
 import psycopg2
 
+conn = psycopg2.connect(dbname='pgvector_python_test')
+conn.autocommit = True
+
+cur = conn.cursor()
+cur.execute('CREATE EXTENSION IF NOT EXISTS vector')
+cur.execute('DROP TABLE IF EXISTS item')
+cur.execute('CREATE TABLE item (id bigserial primary key, factors vector(3))')
+
+register_vector(cur)
+
 
 class TestPsycopg2(object):
     def test_works(self):
-        conn = psycopg2.connect('dbname=pgvector_python_test')
-        conn.autocommit = True
-
-        cur = conn.cursor()
-        cur.execute('CREATE EXTENSION IF NOT EXISTS vector')
-        cur.execute('DROP TABLE IF EXISTS item')
-        cur.execute('CREATE TABLE item (id bigserial primary key, factors vector(3))')
-
-        register_vector(cur)
-
         factors = np.array([1.5, 2, 3])
-        cur.execute("INSERT INTO item (factors) VALUES (%s), (NULL)", (factors,))
+        cur.execute('INSERT INTO item (factors) VALUES (%s), (NULL)', (factors,))
 
-        cur.execute("SELECT * FROM item ORDER BY id")
+        cur.execute('SELECT * FROM item ORDER BY id')
         res = cur.fetchall()
         assert res[0][0] == 1
         assert res[1][0] == 2
--- tests/test_psycopg3.py
+++ tests/test_psycopg3.py
@@ -2,38 +2,48 @@ import numpy as np
 from pgvector.psycopg3 import register_vector
 import psycopg3
 
+conn = psycopg3.connect(dbname='pgvector_python_test')
+conn.autocommit = True
+
+cur = conn.cursor()
+cur.execute('CREATE EXTENSION IF NOT EXISTS vector')
+cur.execute('DROP TABLE IF EXISTS item')
+cur.execute('CREATE TABLE item (id bigserial primary key, factors vector(3))')
+
+register_vector(cur)
+
 
 class TestPsycopg3(object):
+    def setup_method(self, test_method):
+        cur.execute('DELETE FROM item')
+
     def test_works(self):
-        conn = psycopg3.connect('dbname=pgvector_python_test')
-        conn.autocommit = True
-
-        cur = conn.cursor()
-        cur.execute('CREATE EXTENSION IF NOT EXISTS vector')
-        cur.execute('DROP TABLE IF EXISTS item')
-        cur.execute('CREATE TABLE item (id bigserial primary key, factors vector(3))')
-
-        register_vector(cur)
-
         factors = np.array([1.5, 2, 3])
-        cur.execute("INSERT INTO item (factors) VALUES (%s), (NULL)", (factors,))
+        cur.execute('INSERT INTO item (factors) VALUES (%s), (NULL)', (factors,))
 
-        cur.execute("SELECT * FROM item ORDER BY id")
+        cur.execute('SELECT * FROM item ORDER BY id')
         res = cur.fetchall()
-        assert res[0][0] == 1
-        assert res[1][0] == 2
         assert np.array_equal(res[0][1], factors)
         assert res[0][1].dtype == np.float32
         assert res[1][1] is None
 
-        # binary format
-        binary_res = cur.execute("SELECT %b::vector", (factors,)).fetchone()[0]
-        assert np.array_equal(binary_res, factors)
+    def test_binary_format(self):
+        factors = np.array([1.5, 2, 3])
+        res = cur.execute('SELECT %b::vector', (factors,)).fetchone()[0]
+        assert np.array_equal(res, factors)
 
-        # text format
-        text_res = cur.execute("SELECT %t::vector", (factors,)).fetchone()[0]
-        assert np.array_equal(text_res, factors)
+    def test_text_format(self):
+        factors = np.array([1.5, 2, 3])
+        res = cur.execute('SELECT %t::vector', (factors,)).fetchone()[0]
+        assert np.array_equal(res, factors)
 
-        # ensures binary format is correct
-        binary_text_res = cur.execute("SELECT %b::vector::text", (factors,)).fetchone()[0]
-        assert binary_text_res == '[1.5,2,3]'
+    def test_binary_format_correct(self):
+        factors = np.array([1.5, 2, 3])
+        res = cur.execute('SELECT %b::vector::text', (factors,)).fetchone()[0]
+        assert res == '[1.5,2,3]'
+
+    def test_non_contiguous(self):
+        factors = np.flipud(np.array([1.5, 2, 3]))
+        assert not factors.data.contiguous
+        res = cur.execute('SELECT %b::vector', (factors,)).fetchone()[0]
+        assert np.array_equal(res, np.array([3, 2, 1.5]))
