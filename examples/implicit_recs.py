e6c62b18942e7a20dd29acfa4cb5a22c85e0e1d6
Andrew Kane <andrew@ankane.org>
2023-03-31 12:18:41 -0700

    Updated example for SQLAlchemy 2 [skip ci]

--- examples/implicit_recs.py
+++ examples/implicit_recs.py
@@ -1,10 +1,10 @@
 import implicit
 from implicit.datasets.movielens import get_movielens
 from pgvector.sqlalchemy import Vector
-from sqlalchemy import create_engine, text, Column, Integer, String
-from sqlalchemy.orm import declarative_base, Session
+from sqlalchemy import create_engine, select, text, Integer, String
+from sqlalchemy.orm import declarative_base, mapped_column, Session
 
-engine = create_engine('postgresql+psycopg2://localhost/pgvector_example', future=True)
+engine = create_engine('postgresql+psycopg2://localhost/pgvector_example')
 with engine.connect() as conn:
     conn.execute(text('CREATE EXTENSION IF NOT EXISTS vector'))
     conn.commit()
@@ -15,16 +15,16 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True)
-    factors = Column(Vector(20))
+    id = mapped_column(Integer, primary_key=True)
+    factors = mapped_column(Vector(20))
 
 
 class Item(Base):
     __tablename__ = 'item'
 
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    factors = Column(Vector(20))
+    id = mapped_column(Integer, primary_key=True)
+    title = mapped_column(String)
+    factors = mapped_column(Vector(20))
 
 
 Base.metadata.drop_all(engine)
@@ -35,17 +35,17 @@ model = implicit.als.AlternatingLeastSquares(factors=20)
 model.fit(ratings)
 
 users = [dict(id=i, factors=factors) for i, factors in enumerate(model.user_factors)]
-items = [dict(id=i, title=titles[i].decode('utf-8'), factors=factors) for i, factors in enumerate(model.item_factors)]
+items = [dict(id=i, title=titles[i], factors=factors) for i, factors in enumerate(model.item_factors)]
 
 session = Session(engine)
 session.bulk_insert_mappings(User, users)
 session.bulk_insert_mappings(Item, items)
 session.commit()
 
-user = session.query(User).get(1)
-items = session.query(Item).order_by(Item.factors.max_inner_product(user.factors)).limit(5).all()
+user = session.get(User, 1)
+items = session.scalars(select(Item).order_by(Item.factors.max_inner_product(user.factors)).limit(5))
 print('user-based recs:', [item.title for item in items])
 
-item = session.query(Item).filter(Item.title == 'Star Wars (1977)').first()
-items = session.query(Item).filter(Item.id != item.id).order_by(Item.factors.cosine_distance(item.factors)).limit(5).all()
+item = session.scalars(select(Item).filter(Item.title == 'Star Wars (1977)')).first()
+items = session.scalars(select(Item).filter(Item.id != item.id).order_by(Item.factors.cosine_distance(item.factors)).limit(5))
 print('item-based recs:', [item.title for item in items])
