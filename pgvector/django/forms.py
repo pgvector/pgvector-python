6b85e4f812d6df73611d2f2ae16e9969d9fb8a42
Jerin Peter George <jerinpetergeorge@gmail.com>
2023-08-01 09:15:38 +0530

    Remove this commit - 'VectorFormField.has_changed(...)'

--- pgvector/django/forms.py
+++ pgvector/django/forms.py
@@ -5,10 +5,10 @@ from .widgets import VectorWidget
 class VectorFormField(forms.CharField):
     widget = VectorWidget
 
-    def has_changed(self, initial, data):
-        try:
-            initial = initial.tolist()
-        except AttributeError:
-            # initial could be None
-            pass
-        return super().has_changed(initial, data)
+    # def has_changed(self, initial, data):
+    #     try:
+    #         initial = initial.tolist()
+    #     except AttributeError:
+    #         # initial could be None
+    #         pass
+    #     return super().has_changed(initial, data)
