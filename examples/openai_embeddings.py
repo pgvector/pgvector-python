607376856b35f9c4b480289a7cf577d769ebffba
Andrew Kane <andrew@ankane.org>
2023-09-08 15:37:33 -0700

    Updated example [skip ci]

--- examples/openai_embeddings.py
+++ examples/openai_embeddings.py
@@ -8,8 +8,8 @@ conn = psycopg.connect(dbname='pgvector_example', autocommit=True)
 conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
 register_vector(conn)
 
-conn.execute('DROP TABLE IF EXISTS document')
-conn.execute('CREATE TABLE document (id bigserial PRIMARY KEY, content text, embedding vector(1536))')
+conn.execute('DROP TABLE IF EXISTS documents')
+conn.execute('CREATE TABLE documents (id bigserial PRIMARY KEY, content text, embedding vector(1536))')
 
 input = [
     'The dog is barking',
@@ -21,7 +21,7 @@ response = openai.Embedding.create(input=input, model='text-embedding-ada-002')
 embeddings = [v['embedding'] for v in response['data']]
 
 for content, embedding in zip(input, embeddings):
-    conn.execute('INSERT INTO document (content, embedding) VALUES (%s, %s)', (content, embedding))
+    conn.execute('INSERT INTO documents (content, embedding) VALUES (%s, %s)', (content, embedding))
 
 document_id = 1
 neighbors = conn.execute('SELECT content FROM documents WHERE id != %(id)s ORDER BY embedding <=> (SELECT embedding FROM documents WHERE id = %(id)s) LIMIT 5', {'id': document_id}).fetchall()
