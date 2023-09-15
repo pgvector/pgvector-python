d686807348cb2b73492abc19db33ffedc70cbd62
Andrew Kane <andrew@ankane.org>
2023-09-15 11:01:32 -0700

    Added tests for aggregates with Django

--- tests/test_django.py
+++ tests/test_django.py
@@ -2,6 +2,7 @@ import django
 from django.conf import settings
 from django.core import serializers
 from django.db import connection, migrations, models
+from django.db.models import Avg, Sum
 from django.db.migrations.loader import MigrationLoader
 from django.forms import ModelForm
 from math import sqrt
@@ -131,6 +132,22 @@ class TestDjango:
         distance = L2Distance('embedding', [1, 1, 1])
         items = Item.objects.alias(distance=distance).filter(distance__lt=1)
         assert [v.id for v in items] == [1]
+
+    def test_avg(self):
+        avg = Item.objects.aggregate(Avg('embedding'))['embedding__avg']
+        assert avg is None
+        Item(embedding=[1, 2, 3]).save()
+        Item(embedding=[4, 5, 6]).save()
+        avg = Item.objects.aggregate(Avg('embedding'))['embedding__avg']
+        assert np.array_equal(avg, np.array([2.5, 3.5, 4.5]))
+
+    def test_sum(self):
+        avg = Item.objects.aggregate(Sum('embedding'))['embedding__sum']
+        assert avg is None
+        Item(embedding=[1, 2, 3]).save()
+        Item(embedding=[4, 5, 6]).save()
+        avg = Item.objects.aggregate(Sum('embedding'))['embedding__sum']
+        assert np.array_equal(avg, np.array([5, 7, 9]))
 
     def test_serialization(self):
         create_items()
