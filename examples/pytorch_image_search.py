8bd28ca5ef9737ed179c6c0d2caeb69be3d4d124
Andrew Kane <andrew@ankane.org>
2024-03-24 10:36:39 -0700

    Improved example [skip ci]

--- examples/pytorch_image_search.py
+++ examples/pytorch_image_search.py
@@ -8,12 +8,10 @@ from tqdm import tqdm
 
 seed = True
 
-
 # establish connection
 conn = psycopg.connect(dbname='pgvector_example', autocommit=True)
 conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
 register_vector(conn)
-
 
 # load images
 transform = torchvision.transforms.Compose([
@@ -22,7 +20,6 @@ transform = torchvision.transforms.Compose([
 ])
 dataset = torchvision.datasets.CIFAR10(root=tempfile.gettempdir(), train=True, download=True, transform=transform)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=1000)
-
 
 # load pretrained model
 device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
@@ -36,7 +33,7 @@ def generate_embeddings(inputs):
     return model(inputs.to(device)).detach().cpu().numpy()
 
 
-# generate, save, and index embeddings
+# generate and store embeddings
 if seed:
     conn.execute('DROP TABLE IF EXISTS images')
     conn.execute('CREATE TABLE images (id bigserial PRIMARY KEY, embedding vector(512))')
@@ -49,23 +46,24 @@ if seed:
         params = [embedding for embedding in embeddings]
         conn.execute(sql, params)
 
-
-def show_images(dataset_images):
-    grid = torchvision.utils.make_grid(dataset_images)
-    img = (grid / 2 + 0.5).permute(1, 2, 0).numpy()
-    plt.imshow(img)
-    plt.draw()
-    plt.waitforbuttonpress(timeout=3)
-
-
 # load 5 random unseen images
 queryset = torchvision.datasets.CIFAR10(root=tempfile.gettempdir(), train=False, download=True, transform=transform)
 queryloader = torch.utils.data.DataLoader(queryset, batch_size=5, shuffle=True)
 images = next(iter(queryloader))[0]
 
-
 # generate and query embeddings
+results = []
 embeddings = generate_embeddings(images)
 for image, embedding in zip(images, embeddings):
-    result = conn.execute('SELECT id FROM images ORDER BY embedding <=> %s LIMIT 15', (embedding,)).fetchall()
-    show_images([image] + [dataset[row[0] - 1][0] for row in result])
+    result = conn.execute('SELECT id FROM images ORDER BY embedding <=> %s LIMIT 5', (embedding,)).fetchall()
+    nearest_images = [dataset[row[0] - 1][0] for row in result]
+    results.append([image] + nearest_images)
+
+# show images
+fig, axs = plt.subplots(len(results), len(results[0]))
+for i, result in enumerate(results):
+    for j, image in enumerate(result):
+        ax = axs[i, j]
+        ax.imshow((image / 2 + 0.5).permute(1, 2, 0).numpy())
+        ax.set_axis_off()
+plt.show(block=True)
