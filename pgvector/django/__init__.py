3415860599853caad00243060412776e247d3746
Andrew Kane <andrew@ankane.org>
2024-05-16 18:00:52 -0400

    Moved VectorField to separate file for Django [skip ci]

--- pgvector/django/__init__.py
+++ pgvector/django/__init__.py
@@ -1,8 +1,8 @@
 from django.contrib.postgres.operations import CreateExtension
 from django.db.models import Field, FloatField, Func, Value
 import numpy as np
-from .forms import VectorFormField
 from .indexes import IvfflatIndex, HnswIndex
+from .vector import VectorField
 from ..utils import Vector
 
 __all__ = ['VectorExtension', 'VectorField', 'IvfflatIndex', 'HnswIndex', 'L2Distance', 'MaxInnerProduct', 'CosineDistance', 'L1Distance']
@@ -11,54 +11,6 @@ __all__ = ['VectorExtension', 'VectorField', 'IvfflatIndex', 'HnswIndex', 'L2Dis
 class VectorExtension(CreateExtension):
     def __init__(self):
         self.name = 'vector'
-
-
-# https://docs.djangoproject.com/en/4.2/howto/custom-model-fields/
-class VectorField(Field):
-    description = 'Vector'
-    empty_strings_allowed = False
-
-    def __init__(self, *args, dimensions=None, **kwargs):
-        self.dimensions = dimensions
-        super().__init__(*args, **kwargs)
-
-    def deconstruct(self):
-        name, path, args, kwargs = super().deconstruct()
-        if self.dimensions is not None:
-            kwargs['dimensions'] = self.dimensions
-        return name, path, args, kwargs
-
-    def db_type(self, connection):
-        if self.dimensions is None:
-            return 'vector'
-        return 'vector(%d)' % self.dimensions
-
-    def from_db_value(self, value, expression, connection):
-        return Vector.from_db(value)
-
-    def to_python(self, value):
-        if isinstance(value, list):
-            return np.array(value, dtype=np.float32)
-        return Vector.from_db(value)
-
-    def get_prep_value(self, value):
-        return Vector.to_db(value)
-
-    def value_to_string(self, obj):
-        return self.get_prep_value(self.value_from_object(obj))
-
-    def validate(self, value, model_instance):
-        if isinstance(value, np.ndarray):
-            value = value.tolist()
-        super().validate(value, model_instance)
-
-    def run_validators(self, value):
-        if isinstance(value, np.ndarray):
-            value = value.tolist()
-        super().run_validators(value)
-
-    def formfield(self, **kwargs):
-        return super().formfield(form_class=VectorFormField, **kwargs)
 
 
 class DistanceBase(Func):
