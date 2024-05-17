89b9b85a1c912d9f0562cf83153452231bc71a45
Andrew Kane <andrew@ankane.org>
2024-05-16 20:54:06 -0400

    Improved Psycopg 3 tests [skip ci]

--- tests/test_psycopg.py
+++ tests/test_psycopg.py
@@ -6,47 +6,47 @@ import pytest
 conn = psycopg.connect(dbname='pgvector_python_test', autocommit=True)
 
 conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
-conn.execute('DROP TABLE IF EXISTS items')
-conn.execute('CREATE TABLE items (id bigserial PRIMARY KEY, embedding vector(3))')
+conn.execute('DROP TABLE IF EXISTS psycopg_items')
+conn.execute('CREATE TABLE psycopg_items (id bigserial PRIMARY KEY, embedding vector(3), half_embedding halfvec(3), binary_embedding bit(3), sparse_embedding sparsevec(3))')
 
 register_vector(conn)
 
 
 class TestPsycopg:
     def setup_method(self, test_method):
-        conn.execute('DELETE FROM items')
+        conn.execute('DELETE FROM psycopg_items')
 
-    def test_works(self):
+    def test_vector(self):
         embedding = np.array([1.5, 2, 3])
-        conn.execute('INSERT INTO items (embedding) VALUES (%s), (NULL)', (embedding,))
+        conn.execute('INSERT INTO psycopg_items (embedding) VALUES (%s), (NULL)', (embedding,))
 
-        res = conn.execute('SELECT * FROM items ORDER BY id').fetchall()
+        res = conn.execute('SELECT * FROM psycopg_items ORDER BY id').fetchall()
         assert np.array_equal(res[0][1], embedding)
         assert res[0][1].dtype == np.float32
         assert res[1][1] is None
 
-    def test_binary_format(self):
+    def test_vector_binary_format(self):
         embedding = np.array([1.5, 2, 3])
         res = conn.execute('SELECT %b::vector', (embedding,), binary=True).fetchone()[0]
         assert np.array_equal(res, embedding)
 
-    def test_text_format(self):
+    def test_vector_text_format(self):
         embedding = np.array([1.5, 2, 3])
         res = conn.execute('SELECT %t::vector', (embedding,)).fetchone()[0]
         assert np.array_equal(res, embedding)
 
-    def test_binary_format_correct(self):
+    def test_vector_binary_format_correct(self):
         embedding = np.array([1.5, 2, 3])
         res = conn.execute('SELECT %b::vector::text', (embedding,)).fetchone()[0]
         assert res == '[1.5,2,3]'
 
-    def test_text_format_non_contiguous(self):
+    def test_vector_text_format_non_contiguous(self):
         embedding = np.flipud(np.array([1.5, 2, 3]))
         assert not embedding.data.contiguous
         res = conn.execute('SELECT %t::vector', (embedding,)).fetchone()[0]
         assert np.array_equal(res, np.array([3, 2, 1.5]))
 
-    def test_binary_format_non_contiguous(self):
+    def test_vector_binary_format_non_contiguous(self):
         embedding = np.flipud(np.array([1.5, 2, 3]))
         assert not embedding.data.contiguous
         res = conn.execute('SELECT %b::vector', (embedding,)).fetchone()[0]
@@ -55,19 +55,19 @@ class TestPsycopg:
     def test_text_copy(self):
         embedding = np.array([1.5, 2, 3])
         cur = conn.cursor()
-        with cur.copy("COPY items (embedding) FROM STDIN") as copy:
-            copy.write_row([embedding])
+        with cur.copy("COPY psycopg_items (embedding, half_embedding, binary_embedding, sparse_embedding) FROM STDIN") as copy:
+            copy.write_row([embedding, HalfVec(embedding), '101', SparseVec.from_dense(embedding)])
 
     def test_binary_copy(self):
         embedding = np.array([1.5, 2, 3])
         cur = conn.cursor()
-        with cur.copy("COPY items (embedding) FROM STDIN WITH (FORMAT BINARY)") as copy:
+        with cur.copy("COPY psycopg_items (embedding) FROM STDIN WITH (FORMAT BINARY)") as copy:
             copy.write_row([embedding])
 
     def test_binary_copy_set_types(self):
         embedding = np.array([1.5, 2, 3])
         cur = conn.cursor()
-        with cur.copy("COPY items (id, embedding) FROM STDIN WITH (FORMAT BINARY)") as copy:
+        with cur.copy("COPY psycopg_items (id, embedding) FROM STDIN WITH (FORMAT BINARY)") as copy:
             copy.set_types(['int8', 'vector'])
             copy.write_row([1, embedding])
 
@@ -127,16 +127,16 @@ class TestPsycopg:
         conn = await psycopg.AsyncConnection.connect(dbname='pgvector_python_test', autocommit=True)
 
         await conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
-        await conn.execute('DROP TABLE IF EXISTS items')
-        await conn.execute('CREATE TABLE items (id bigserial PRIMARY KEY, embedding vector(3))')
+        await conn.execute('DROP TABLE IF EXISTS psycopg_items')
+        await conn.execute('CREATE TABLE psycopg_items (id bigserial PRIMARY KEY, embedding vector(3))')
 
         await register_vector_async(conn)
 
         embedding = np.array([1.5, 2, 3])
-        await conn.execute('INSERT INTO items (embedding) VALUES (%s), (NULL)', (embedding,))
+        await conn.execute('INSERT INTO psycopg_items (embedding) VALUES (%s), (NULL)', (embedding,))
 
         async with conn.cursor() as cur:
-            await cur.execute('SELECT * FROM items ORDER BY id')
+            await cur.execute('SELECT * FROM psycopg_items ORDER BY id')
             res = await cur.fetchall()
             assert np.array_equal(res[0][1], embedding)
             assert res[0][1].dtype == np.float32
