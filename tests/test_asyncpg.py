5c8711537e8202bc438dffde15e7d0560367c96f
Andrew Kane <andrew@ankane.org>
2024-05-16 21:48:08 -0400

    Updated table name for asyncpg tests [skip ci]

--- tests/test_asyncpg.py
+++ tests/test_asyncpg.py
@@ -10,15 +10,15 @@ class TestAsyncpg:
     async def test_vector(self):
         conn = await asyncpg.connect(database='pgvector_python_test')
         await conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
-        await conn.execute('DROP TABLE IF EXISTS items')
-        await conn.execute('CREATE TABLE items (id bigserial PRIMARY KEY, embedding vector(3))')
+        await conn.execute('DROP TABLE IF EXISTS asyncpg_items')
+        await conn.execute('CREATE TABLE asyncpg_items (id bigserial PRIMARY KEY, embedding vector(3))')
 
         await register_vector(conn)
 
         embedding = np.array([1.5, 2, 3])
-        await conn.execute("INSERT INTO items (embedding) VALUES ($1), (NULL)", embedding)
+        await conn.execute("INSERT INTO asyncpg_items (embedding) VALUES ($1), (NULL)", embedding)
 
-        res = await conn.fetch("SELECT * FROM items ORDER BY id")
+        res = await conn.fetch("SELECT * FROM asyncpg_items ORDER BY id")
         assert res[0]['id'] == 1
         assert res[1]['id'] == 2
         assert np.array_equal(res[0]['embedding'], embedding)
@@ -26,7 +26,7 @@ class TestAsyncpg:
         assert res[1]['embedding'] is None
 
         # ensures binary format is correct
-        text_res = await conn.fetch("SELECT embedding::text FROM items ORDER BY id LIMIT 1")
+        text_res = await conn.fetch("SELECT embedding::text FROM asyncpg_items ORDER BY id LIMIT 1")
         assert text_res[0]['embedding'] == '[1.5,2,3]'
 
         await conn.close()
@@ -35,22 +35,22 @@ class TestAsyncpg:
     async def test_halfvec(self):
         conn = await asyncpg.connect(database='pgvector_python_test')
         await conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
-        await conn.execute('DROP TABLE IF EXISTS items')
-        await conn.execute('CREATE TABLE items (id bigserial PRIMARY KEY, embedding halfvec(3))')
+        await conn.execute('DROP TABLE IF EXISTS asyncpg_items')
+        await conn.execute('CREATE TABLE asyncpg_items (id bigserial PRIMARY KEY, embedding halfvec(3))')
 
         await register_vector(conn)
 
         embedding = [1.5, 2, 3]
-        await conn.execute("INSERT INTO items (embedding) VALUES ($1), (NULL)", embedding)
+        await conn.execute("INSERT INTO asyncpg_items (embedding) VALUES ($1), (NULL)", embedding)
 
-        res = await conn.fetch("SELECT * FROM items ORDER BY id")
+        res = await conn.fetch("SELECT * FROM asyncpg_items ORDER BY id")
         assert res[0]['id'] == 1
         assert res[1]['id'] == 2
         assert res[0]['embedding'].to_list() == [1.5, 2, 3]
         assert res[1]['embedding'] is None
 
         # ensures binary format is correct
-        text_res = await conn.fetch("SELECT embedding::text FROM items ORDER BY id LIMIT 1")
+        text_res = await conn.fetch("SELECT embedding::text FROM asyncpg_items ORDER BY id LIMIT 1")
         assert text_res[0]['embedding'] == '[1.5,2,3]'
 
         await conn.close()
@@ -59,22 +59,22 @@ class TestAsyncpg:
     async def test_bit(self):
         conn = await asyncpg.connect(database='pgvector_python_test')
         await conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
-        await conn.execute('DROP TABLE IF EXISTS items')
-        await conn.execute('CREATE TABLE items (id bigserial PRIMARY KEY, embedding bit(3))')
+        await conn.execute('DROP TABLE IF EXISTS asyncpg_items')
+        await conn.execute('CREATE TABLE asyncpg_items (id bigserial PRIMARY KEY, embedding bit(3))')
 
         await register_vector(conn)
 
         embedding = asyncpg.BitString.from_int(5, length=3)
-        await conn.execute("INSERT INTO items (embedding) VALUES ($1), (NULL)", embedding)
+        await conn.execute("INSERT INTO asyncpg_items (embedding) VALUES ($1), (NULL)", embedding)
 
-        res = await conn.fetch("SELECT * FROM items ORDER BY id")
+        res = await conn.fetch("SELECT * FROM asyncpg_items ORDER BY id")
         assert res[0]['id'] == 1
         assert res[1]['id'] == 2
         assert res[0]['embedding'].to_int() == 5
         assert res[1]['embedding'] is None
 
         # ensures binary format is correct
-        text_res = await conn.fetch("SELECT embedding::text FROM items ORDER BY id LIMIT 1")
+        text_res = await conn.fetch("SELECT embedding::text FROM asyncpg_items ORDER BY id LIMIT 1")
         assert text_res[0]['embedding'] == '101'
 
         await conn.close()
@@ -83,22 +83,22 @@ class TestAsyncpg:
     async def test_sparsevec(self):
         conn = await asyncpg.connect(database='pgvector_python_test')
         await conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
-        await conn.execute('DROP TABLE IF EXISTS items')
-        await conn.execute('CREATE TABLE items (id bigserial PRIMARY KEY, embedding sparsevec(3))')
+        await conn.execute('DROP TABLE IF EXISTS asyncpg_items')
+        await conn.execute('CREATE TABLE asyncpg_items (id bigserial PRIMARY KEY, embedding sparsevec(3))')
 
         await register_vector(conn)
 
         embedding = SparseVec.from_dense([1.5, 2, 3])
-        await conn.execute("INSERT INTO items (embedding) VALUES ($1), (NULL)", embedding)
+        await conn.execute("INSERT INTO asyncpg_items (embedding) VALUES ($1), (NULL)", embedding)
 
-        res = await conn.fetch("SELECT * FROM items ORDER BY id")
+        res = await conn.fetch("SELECT * FROM asyncpg_items ORDER BY id")
         assert res[0]['id'] == 1
         assert res[1]['id'] == 2
         assert res[0]['embedding'].to_dense() == [1.5, 2, 3]
         assert res[1]['embedding'] is None
 
         # ensures binary format is correct
-        text_res = await conn.fetch("SELECT embedding::text FROM items ORDER BY id LIMIT 1")
+        text_res = await conn.fetch("SELECT embedding::text FROM asyncpg_items ORDER BY id LIMIT 1")
         assert text_res[0]['embedding'] == '{1:1.5,2:2,3:3}/3'
 
         await conn.close()
@@ -112,13 +112,13 @@ class TestAsyncpg:
 
         async with pool.acquire() as conn:
             await conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
-            await conn.execute('DROP TABLE IF EXISTS items')
-            await conn.execute('CREATE TABLE items (id bigserial PRIMARY KEY, embedding vector(3))')
+            await conn.execute('DROP TABLE IF EXISTS asyncpg_items')
+            await conn.execute('CREATE TABLE asyncpg_items (id bigserial PRIMARY KEY, embedding vector(3))')
 
             embedding = np.array([1.5, 2, 3])
-            await conn.execute("INSERT INTO items (embedding) VALUES ($1), (NULL)", embedding)
+            await conn.execute("INSERT INTO asyncpg_items (embedding) VALUES ($1), (NULL)", embedding)
 
-            res = await conn.fetch("SELECT * FROM items ORDER BY id")
+            res = await conn.fetch("SELECT * FROM asyncpg_items ORDER BY id")
             assert res[0]['id'] == 1
             assert res[1]['id'] == 2
             assert np.array_equal(res[0]['embedding'], embedding)
