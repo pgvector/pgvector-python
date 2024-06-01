c1a7312a3509932b2c39e730a7906fd44c86015c
Andrew Kane <andrew@ankane.org>
2024-06-01 16:04:29 -0700

    Improved bit code [skip ci]

--- pgvector/utils/bit.py
+++ pgvector/utils/bit.py
@@ -7,28 +7,30 @@ class Bit:
         if isinstance(value, bytes):
             count = unpack_from('>i', value)[0]
             buf = np.frombuffer(value[4:], dtype=np.uint8)
-            self.value = np.unpackbits(buf, count=count).astype(bool)
+            self._value = np.unpackbits(buf, count=count).astype(bool)
         elif isinstance(value, str):
-            self.value = np.array([v != '0' for v in value], dtype=bool)
+            self._value = np.array([v != '0' for v in value], dtype=bool)
         else:
-            self.value = np.array(value, dtype=bool)
+            self._value = np.array(value, dtype=bool)
 
     def __str__(self):
-        return self.__class__.to_db(self)
+        return ''.join(self._value.astype(np.uint8).astype(str))
 
     def __repr__(self):
         return f'Bit({self})'
+
+    def to_binary(self):
+        value = self._value
+        return pack('>i', len(value)) + np.packbits(value).tobytes()
 
     def to_db(value):
         if not isinstance(value, Bit):
             raise ValueError('expected bit')
 
-        value = value.value
-        return ''.join(value.astype(np.uint8).astype(str))
+        return str(value)
 
     def to_db_binary(value):
         if not isinstance(value, Bit):
             raise ValueError('expected bit')
 
-        value = value.value
-        return pack('>i', len(value)) + np.packbits(value).tobytes()
+        return value.to_binary()
