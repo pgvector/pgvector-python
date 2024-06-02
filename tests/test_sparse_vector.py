7e17e9e3091abb168b89289acc36dff5531a6e50
Andrew Kane <andrew@ankane.org>
2024-06-01 19:22:11 -0700

    Added test for __repr__ [skip ci]

--- tests/test_sparse_vector.py
+++ tests/test_sparse_vector.py
@@ -7,3 +7,6 @@ class TestSparseVector:
     def test_from_dense(self):
         assert SparseVector.from_dense([1, 2, 3]).to_list() == [1, 2, 3]
         assert SparseVector.from_dense([1, 2, 3]).to_numpy().tolist() == [1, 2, 3]
+
+    def test_repr(self):
+        assert repr(SparseVector.from_dense([1, 2, 3])) == 'SparseVector(3, [0, 1, 2], [1, 2, 3])'
