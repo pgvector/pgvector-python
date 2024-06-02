85ea3573184fad34bfd2e16df2abccf6a16ece7f
Andrew Kane <andrew@ankane.org>
2024-06-01 17:43:27 -0700

    Added tests for ndim [skip ci]

--- tests/test_half_vector.py
+++ tests/test_half_vector.py
@@ -24,3 +24,13 @@ class TestHalfVector:
         arr = np.array([1, 2, 3], dtype='>f2')
         assert HalfVector(arr).to_list() == [1, 2, 3]
         assert HalfVector(arr).to_numpy() is arr
+
+    def test_ndim_two(self):
+        with pytest.raises(ValueError) as error:
+            HalfVector([[1, 2], [3, 4]])
+        assert str(error.value) == 'expected ndim to be 1'
+
+    def test_ndim_zero(self):
+        with pytest.raises(ValueError) as error:
+            HalfVector(1)
+        assert str(error.value) == 'expected ndim to be 1'
