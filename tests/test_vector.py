241b087e797e84a3a585fb5b6261a6e706602a87
Andrew Kane <andrew@ankane.org>
2024-06-01 19:03:09 -0700

    Added tests for Vector class [skip ci]

---
+++ tests/test_vector.py
@@ -0,0 +1,38 @@
+import numpy as np
+from pgvector.utils import Vector
+import pytest
+
+
+class TestVector:
+    def test_list(self):
+        assert Vector([1, 2, 3]).to_list() == [1, 2, 3]
+
+    def test_list_str(self):
+        with pytest.raises(ValueError, match='could not convert string to float') as error:
+            Vector([1, 'two', 3])
+
+    def test_tuple(self):
+        assert Vector((1, 2, 3)).to_list() == [1, 2, 3]
+
+    def test_ndarray(self):
+        arr = np.array([1, 2, 3])
+        assert Vector(arr).to_list() == [1, 2, 3]
+        assert Vector(arr).to_numpy() is not arr
+
+    def test_ndarray_same_object(self):
+        arr = np.array([1, 2, 3], dtype='>f4')
+        assert Vector(arr).to_list() == [1, 2, 3]
+        assert Vector(arr).to_numpy() is arr
+
+    def test_ndim_two(self):
+        with pytest.raises(ValueError) as error:
+            Vector([[1, 2], [3, 4]])
+        assert str(error.value) == 'expected ndim to be 1'
+
+    def test_ndim_zero(self):
+        with pytest.raises(ValueError) as error:
+            Vector(1)
+        assert str(error.value) == 'expected ndim to be 1'
+
+    def test_repr(self):
+        assert repr(Vector([1, 2, 3])) == 'Vector([1.0, 2.0, 3.0])'
