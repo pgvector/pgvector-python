3a4118ca3687a5ff366087924b0aacb523bab66e
Andrew Kane <andrew@ankane.org>
2024-06-04 15:26:07 -0700

    Improved SparseVector tests [skip ci]

--- tests/test_sparse_vector.py
+++ tests/test_sparse_vector.py
@@ -5,12 +5,12 @@ import pytest
 
 class TestSparseVector:
     def test_from_dense(self):
-        assert SparseVector.from_dense([1, 2, 3]).to_list() == [1, 2, 3]
-        assert SparseVector.from_dense([1, 2, 3]).to_numpy().tolist() == [1, 2, 3]
+        assert SparseVector.from_dense([1, 0, 2, 0, 3, 0]).to_list() == [1, 0, 2, 0, 3, 0]
+        assert SparseVector.from_dense([1, 0, 2, 0, 3, 0]).to_numpy().tolist() == [1, 0, 2, 0, 3, 0]
 
     def test_repr(self):
-        assert repr(SparseVector.from_dense([1, 2, 3])) == 'SparseVector(3, [0, 1, 2], [1.0, 2.0, 3.0])'
-        assert str(SparseVector.from_dense([1, 2, 3])) == 'SparseVector(3, [0, 1, 2], [1.0, 2.0, 3.0])'
+        assert repr(SparseVector.from_dense([1, 0, 2, 0, 3, 0])) == 'SparseVector(6, [0, 2, 4], [1.0, 2.0, 3.0])'
+        assert str(SparseVector.from_dense([1, 0, 2, 0, 3, 0])) == 'SparseVector(6, [0, 2, 4], [1.0, 2.0, 3.0])'
 
     def test_dim(self):
-        assert SparseVector.from_dense([1, 2, 3]).dim() == 3
+        assert SparseVector.from_dense([1, 0, 2, 0, 3, 0]).dim() == 6
