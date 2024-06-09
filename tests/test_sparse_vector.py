33eb182c8cbb638f4b6266e0518b14d0beed21d8
Andrew Kane <andrew@ankane.org>
2024-06-09 13:50:55 -0700

    Added more tests [skip ci]

--- tests/test_sparse_vector.py
+++ tests/test_sparse_vector.py
@@ -16,6 +16,8 @@ class TestSparseVector:
     def test_from_sparse(self):
         arr = coo_array(np.array([1, 0, 2, 0, 3, 0]))
         assert SparseVector.from_sparse(arr).to_list() == [1, 0, 2, 0, 3, 0]
+        assert SparseVector.from_sparse(arr.tocsc()).to_list() == [1, 0, 2, 0, 3, 0]
+        assert SparseVector.from_sparse(arr.tocsr()).to_list() == [1, 0, 2, 0, 3, 0]
         assert SparseVector.from_sparse(arr.todok()).to_list() == [1, 0, 2, 0, 3, 0]
 
     def test_repr(self):
