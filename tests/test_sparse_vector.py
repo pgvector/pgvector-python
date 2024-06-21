ae8402fbc122a7d496007bb3bb62b611956025d2
Andrew Kane <andrew@ankane.org>
2024-06-20 18:49:26 -0700

    Improved tests [skip ci]

--- tests/test_sparse_vector.py
+++ tests/test_sparse_vector.py
@@ -5,15 +5,18 @@ from scipy.sparse import coo_array
 
 
 class TestSparseVector:
-    def test_from_dense(self):
-        assert SparseVector([1, 0, 2, 0, 3, 0]).to_list() == [1, 0, 2, 0, 3, 0]
-        assert SparseVector([1, 0, 2, 0, 3, 0]).to_numpy().tolist() == [1, 0, 2, 0, 3, 0]
-        assert SparseVector(np.array([1, 0, 2, 0, 3, 0])).to_list() == [1, 0, 2, 0, 3, 0]
+    def test_from_list(self):
+        vec = SparseVector([1, 0, 2, 0, 3, 0])
+        assert vec.to_list() == [1, 0, 2, 0, 3, 0]
+        assert vec.to_numpy().tolist() == [1, 0, 2, 0, 3, 0]
 
-    def test_from_dense_dimensions(self):
+    def test_from_list_dimensions(self):
         with pytest.raises(ValueError) as error:
             SparseVector([1, 0, 2, 0, 3, 0], 6)
         assert str(error.value) == 'dimensions not allowed'
+
+    def test_from_numpy(self):
+        assert SparseVector(np.array([1, 0, 2, 0, 3, 0])).to_list() == [1, 0, 2, 0, 3, 0]
 
     def test_from_dict(self):
         vec = SparseVector({2: 2, 4: 3, 0: 1, 3: 0}, 6)
@@ -25,12 +28,12 @@ class TestSparseVector:
             SparseVector({0: 1, 2: 2, 4: 3})
         assert str(error.value) == 'dimensions required'
 
-    def test_from_sparse(self):
+    def test_from_scipy(self):
         arr = coo_array(np.array([1, 0, 2, 0, 3, 0]))
         assert SparseVector(arr).to_list() == [1, 0, 2, 0, 3, 0]
         assert SparseVector(arr.todok()).to_list() == [1, 0, 2, 0, 3, 0]
 
-    def test_from_sparse_dimensions(self):
+    def test_from_scipy_dimensions(self):
         with pytest.raises(ValueError) as error:
             SparseVector(coo_array(np.array([1, 0, 2, 0, 3, 0])), 6)
         assert str(error.value) == 'dimensions not allowed'
