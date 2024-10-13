7887a34d5f1914592fcbd52be13b18b7ed43d39e
Andrew Kane <andrew@ankane.org>
2024-10-13 15:51:16 -0700

    Added test for arrays with asyncpg

--- tests/test_asyncpg.py
+++ tests/test_asyncpg.py
@@ -95,6 +95,24 @@ class TestAsyncpg:
         await conn.close()
 
     @pytest.mark.asyncio
+    async def test_vector_array(self):
+        conn = await asyncpg.connect(database='pgvector_python_test')
+        await conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
+        await conn.execute('DROP TABLE IF EXISTS asyncpg_items')
+        await conn.execute('CREATE TABLE asyncpg_items (id bigserial PRIMARY KEY, embeddings vector[])')
+
+        await register_vector(conn)
+
+        embeddings = [np.array([1.5, 2, 3]), np.array([4.5, 5, 6])]
+        await conn.execute("INSERT INTO asyncpg_items (embeddings) VALUES (ARRAY[$1, $2]::vector[])", embeddings[0], embeddings[1])
+
+        res = await conn.fetch("SELECT * FROM asyncpg_items ORDER BY id")
+        assert np.array_equal(res[0]['embeddings'][0], embeddings[0])
+        assert np.array_equal(res[0]['embeddings'][1], embeddings[1])
+
+        await conn.close()
+
+    @pytest.mark.asyncio
     async def test_pool(self):
         async def init(conn):
             await register_vector(conn)
