1b9df46f9542f3262f6c93a1a858c1414a0ffdc5
Andrew Kane <andrew@ankane.org>
2025-02-08 19:38:38 -0800

    Improved test code [skip ci]

--- tests/test_sqlmodel.py
+++ tests/test_sqlmodel.py
@@ -1,9 +1,8 @@
 import numpy as np
 from pgvector.sqlalchemy import VECTOR, HALFVEC, BIT, SPARSEVEC, SparseVector, avg, sum
 import pytest
-from sqlalchemy import Column, Index
 from sqlalchemy.exc import StatementError
-from sqlmodel import Field, Session, SQLModel, create_engine, delete, select, text
+from sqlmodel import Field, Index, Session, SQLModel, create_engine, delete, select, text
 from typing import Any, Optional
 
 engine = create_engine('postgresql+psycopg2://localhost/pgvector_python_test')
