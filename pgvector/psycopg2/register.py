37b148f459863ee6f6c448fb93eecef569e7eb40
Andrew Kane <andrew@ankane.org>
2025-02-09 20:29:04 -0800

    Removed default value [skip ci]

--- pgvector/psycopg2/register.py
+++ pgvector/psycopg2/register.py
@@ -5,11 +5,10 @@ from .sparsevec import register_sparsevec_info
 from .vector import register_vector_info
 
 
-# TODO remove default value for conn_or_curs in 0.4.0
 # TODO make globally False by default in 0.4.0
 # note: register_adapter is always global
 # TODO make arrays True by defalt in 0.4.0
-def register_vector(conn_or_curs=None, globally=True, arrays=False):
+def register_vector(conn_or_curs, globally=True, arrays=False):
     conn = conn_or_curs if hasattr(conn_or_curs, 'cursor') else conn_or_curs.connection
     cur = conn.cursor(cursor_factory=cursor)
     scope = None if globally else conn_or_curs
