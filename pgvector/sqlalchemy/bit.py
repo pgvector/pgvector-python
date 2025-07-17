2b5e53d0faf8e331237d509cad16863a726a4dd1
Giacomo rua <giacomo.rua@outlook.com>
2025-07-17 17:52:42 +0200

    feat: add processor methods to BIT class

--- pgvector/sqlalchemy/bit.py
+++ pgvector/sqlalchemy/bit.py
@@ -1,6 +1,8 @@
+import asyncpg
+from sqlalchemy.dialects.postgresql.asyncpg import PGDialect_asyncpg
 from sqlalchemy.dialects.postgresql.base import ischema_names
 from sqlalchemy.types import UserDefinedType, Float
-
+from .. import Bit
 
 class BIT(UserDefinedType):
     cache_ok = True
@@ -13,6 +15,21 @@ class BIT(UserDefinedType):
         if self.length is None:
             return 'BIT'
         return 'BIT(%d)' % self.length
+
+    def bind_processor(self, dialect):
+        def process(value):
+            value = Bit._to_db(value)
+            if value and isinstance(dialect, PGDialect_asyncpg): 
+                return asyncpg.BitString(value)
+            return value
+        return process
+    
+    def result_processor(self, dialect, coltype):
+        def process(value): 
+            if value and isinstance(dialect, PGDialect_asyncpg):
+                return value.as_string()
+            return Bit._from_db(value).to_text() 
+        return process
 
     class comparator_factory(UserDefinedType.Comparator):
         def hamming_distance(self, other):
